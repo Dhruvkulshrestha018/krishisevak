da15b63cd2adfc8489b2b3af1b6da08f7abe2837
Dhruvkulshrestha018 <dhruvkul2004@gmail.com>
2026-06-24 08:53:00 +0530

    logging and exception added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Dhruv Kulshrestha",
+    author_email="dhruvkul2004@gmail.com",
+    packages=find_packages()
+)
